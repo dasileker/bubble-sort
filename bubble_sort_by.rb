3111c7dccfaa1309d4485ab71aa37c7deeb575f9
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-09-07 11:18:02 +0200

    impliment bubble_sort

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,1 +1,14 @@
-puts "testing"
+def bubble_sort(array)
+    for i in 0..array.length-2
+        for j in 0..array.length-2
+            if array[j] > array[j+1]
+                array[j], array[j+1] = array[j+1], array[j]            
+            end
+        end
+    end
+     return array
+end
+
+array = [2, 4, 5, 7, 712, 64]
+
+p bubble_sort(array)
