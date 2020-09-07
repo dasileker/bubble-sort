76b6ef2a484a24e71ce1e535416468d865e07656
dasileker <dasileker@gmail.com>
2020-09-07 13:20:56 +0100

    added bubble_sort_by code

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,4 +1,4 @@
-def bubble_sort_by(arr)
+def bubble_sort(arr)
 
   len = arr.length()
 
@@ -12,4 +12,27 @@ def bubble_sort_by(arr)
     return arr
 end
 
-p bubble_sort_by([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
+
+
+#bubble_sort_by[""hi","hello","hey""]
+def bubble_sort_by(array)
+  raise 'No block_given' unless block_given?
+
+  len = array.length - 1
+  len.step(1, -1) do |a|
+    (0...a).each do |i|
+      test = yield(array[i], array[i + 1])
+      next unless test.positive?
+
+      temp = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = temp
+    end
+  end
+  puts array.join(' ')
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
